4b9f9d911489926dfce421c0ceefa83a80e7f9a2
Jeremy Nelson <jermnelson.gmail.com>
2015-05-13 11:14:25 -0600

    Added content and navigation

--- __init__.py
+++ __init__.py
@@ -4,9 +4,15 @@ __license__ = "GPLv3"
 from flask import Flask, render_template
 app = Flask(__name__)
 
+@app.route("/<path:name>")
+def page(name):
+    if name.endswith("/"):
+        name = name[:-1]
+    return render_template("{}.html".format(name))
+
 @app.route("/")
 def main():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=20144, debug=True)
+    app.run(host='0.0.0.0', port=20154, debug=True)
